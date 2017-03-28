40326d0ffdbb91e276bcab5412252068dfc4d492
George Studenko <gnstudenko@gmail.com>
2017-03-28 19:31:02 +0200

    feat: Add Details action method

--- src/ASP.NET MVC Comic Book Gallery/Controllers/ComicBooksController.cs
+++ src/ASP.NET MVC Comic Book Gallery/Controllers/ComicBooksController.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ASP.NET_MVC_Comic_Book_Gallery.Controllers
 {
-    public class ComicBooksController : Controllers
+    public class ComicBooksController : Controller
     {
-
+        public string Detail() {
+            return "Welcome to the details page";
+        }
 
     }
 }
