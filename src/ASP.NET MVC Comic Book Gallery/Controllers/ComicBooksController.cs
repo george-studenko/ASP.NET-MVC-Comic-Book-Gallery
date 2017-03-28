acb2d148337c8573fa64280357de979957db3f96
George Studenko <gnstudenko@gmail.com>
2017-03-28 20:01:43 +0200

    feat: Add Comic Book Details View

--- src/ASP.NET MVC Comic Book Gallery/Controllers/ComicBooksController.cs
+++ src/ASP.NET MVC Comic Book Gallery/Controllers/ComicBooksController.cs
@@ -9,13 +9,7 @@ namespace ASP.NET_MVC_Comic_Book_Gallery.Controllers
     public class ComicBooksController : Controller
     {
         public ActionResult Detail() {
-
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-
-            return Content ("Hello from the details page");
+            return View();
         }
 
     }
