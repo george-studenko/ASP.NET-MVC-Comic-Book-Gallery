36c640a1f734e9b30de7e83612b7bfaee911e892
George Studenko <gnstudenko@gmail.com>
2017-03-28 19:27:05 +0200

    feat: Add ComicBooksController

---
+++ src/ASP.NET MVC Comic Book Gallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC_Comic_Book_Gallery.Controllers
+{
+    public class ComicBooksController : Controllers
+    {
+
+
+    }
+}
